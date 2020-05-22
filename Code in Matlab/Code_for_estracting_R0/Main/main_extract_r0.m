8941fba7e3dbd6a266d5adba2b63a4906c2a3d04
dabol99 <dabol99@gmail.com>
2020-05-22 09:48:12 +0900

    Added function to generate initial points for the minimization
    I changed the main code so that now it calls a function to generate the initial points for the minimization.

--- Code in Matlab/Code_for_estracting_R0/Main/main_extract_r0.m
+++ Code in Matlab/Code_for_estracting_R0/Main/main_extract_r0.m
@@ -2,57 +2,52 @@
 okinawa_data = import_okinawa_data();
 % cut data before the 2nd of april (remove 24 rows)
 okinawa_data = okinawa_data(24 : end, :);
-initial_confirmed_infected = NaN; 
 
-
-% SEIR parameters from Ferretti
-SEIR_metaparameters = initialize_SEIR_metaparameters(initial_confirmed_infected);
+% Initialize SEIR parameters from Ferretti without an estimate for the
+% initial number of infected people
+SEIR_metaparameters = initialize_SEIR_metaparameters(NaN);
 
 % define simulation time and total confirmed cases based on imported data
-time = okinawa_data.date;% - round(mean(SEIR_metaparameters.incubation_time_distribution));
+time = okinawa_data.date;
 tspan = 1 : length(time);
 SEIR_metaparameters.tspan = tspan;
+
+% Add a smoothened curve of the cumulative confirmed cases to SEIR
+% metaparameters struct
 moving_window = 4;
 SEIR_metaparameters.cumulative_confirmed_cases = movmean(okinawa_data.testedPositive, moving_window);
 
-% override of 
+% initialize parameter bounds and initial points for the optimization
+SEIR_metaparameters.single_variable_grid_size = 2;
+SEIR_metaparameters.random_points = 100;
+total_points = 6^SEIR_metaparameters.single_variable_grid_size + SEIR_metaparameters.random_points;
+[lower_bounds, upper_bounds, initial_points] = generate_parameter_constraints_and_initial_points(SEIR_metaparameters);
 
-
-% initialize one SEIR simulation batch and compute the mean square
-% deviation from the observed values.
-R0_value = 2;
-time_of_suppression = 1; % note, that you have to add the incubation time to get the real time
-R0_suppression = 0.95;
-estimated_initial_infected = 1;
-estimated_initial_exposed = 1;
-mean_detection_percentage = 0.1;
-R0_variables = [R0_value, time_of_suppression, R0_suppression, estimated_initial_infected, estimated_initial_exposed, mean_detection_percentage];
-
-lower_bound = [1.5, 1, 0, 1, 0, 0.07];
-upper_bound = [5, length(time), 1, 500, 500, 0.2];
-grid_initial_conditions = ndspace(lower_bound, upper_bound, 3);
-random_points = 1000;
-random_initial_conditions = repmat(lower_bound, random_points, 1) + rand(random_points, 6) .* (repmat(upper_bound, random_points, 1) - repmat(lower_bound, random_points, 1));
-initial_conditions = [grid_initial_conditions; random_initial_conditions];
-
+% initialize linear constraints.
+% First line it to have that the initial exposed are not more than 20 times
+% the initial infected
 Aineq = [0 0 0 -20 +1, 0];
 bineq = 0;
 
+% perform the optimization
 tic
-% optimization
-problem = createOptimProblem('fmincon', 'x0',R0_variables, 'objective', @(y) compute_deviations_from_real_cases(y, SEIR_metaparameters), 'lb', lower_bound, 'ub', upper_bound, 'Aineq', Aineq, 'bineq', bineq);
-initial_points = CustomStartPointSet(initial_conditions);
+problem = createOptimProblem('fmincon', 'x0', lower_bounds, 'objective', @(y) compute_deviations_from_real_cases(y, SEIR_metaparameters), 'lb', lower_bounds, 'ub', upper_bounds, 'Aineq', Aineq, 'bineq', bineq);
 R0_variables = run(MultiStart, problem, initial_points);
 toc
 
+% compute SEIR with optimal parameters
 [t_optimal, cumulative_detected_cases_optimal] = wrapper_to_compute_SEIR(R0_variables, SEIR_metaparameters);
 optimal_deviation = compute_deviations_from_real_cases(R0_variables, SEIR_metaparameters);
 
-% plot of results
+% plot of the Okinawa data and the optimal SEIR simulation
+Okinawa_state_of_emergency = datetime(2020, 04, 22);
+observation_delay = mean(SEIR_metaparameters.incubation_time_distribution);
+
 figure(15)
-plot(time, SEIR_metaparameters.cumulative_confirmed_cases, 'r', 'DisplayName', 'Okinawa data')
+plot(time + observation_delay, SEIR_metaparameters.cumulative_confirmed_cases, 'r', 'DisplayName', 'Okinawa data')
 hold all
-plot(time, cumulative_detected_cases_optimal, 'DisplayName', 'predictions')
+plot(time + observation_delay, cumulative_detected_cases_optimal, 'DisplayName', 'predictions')
+plot([Okinawa_state_of_emergency, Okinawa_state_of_emergency], [min(cumulative_detected_cases_optimal), max(cumulative_detected_cases_optimal)])
 hold off
 legend('show')
 
