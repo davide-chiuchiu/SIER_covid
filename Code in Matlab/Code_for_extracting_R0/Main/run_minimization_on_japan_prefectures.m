7b128f7b0ef557c55bc6f09104dba0c4ab916e5e
dabol99 <dabol99@gmail.com>
2020-05-26 13:51:23 +0900

    Added function to initialize minimization on all japanese prefectures
    I added the script run_minimization_on_japan_prefectures to run the minimization over all japanese prefectures and gather some statistics. 
    
    I do not know the results yet as I need to run this script on my desktop to speed up the code enough

--- Code in Matlab/Code_for_extracting_R0/Main/run_minimization_on_japan_prefectures.m
+++ Code in Matlab/Code_for_extracting_R0/Main/run_minimization_on_japan_prefectures.m
@@ -1,21 +1,35 @@
+num_initial_points = 20;
+
 % list of files 
-files_to_load = dir('Data/Infection_data_all_japanese_prefectures/J*');
+files_to_load = struct2table(dir('Data/Infection_data_all_japanese_prefectures/J*'));
 
-
-% to create the file location
-[files_to_load(1).folder '/' files_to_load(1).name];
-
-
-% load file containing the population data
+% load file containing the population data 
 population_data = readtable('Data/Infection_data_all_japanese_prefectures/populationData.csv');
+population_data.name = strcat(population_data.name, '.csv');
+
+% combine the information about files to load and population
+combined_information = join(population_data, files_to_load);
+
+% create table with outputs
+outputs_table = table('Size', [length(combined_information.name), 4], 'VariableTypes', {'double','double','datetime', 'datetime'}, 'VariableNames', {'R0', 'R0_suppression', 'start_date', 'suppression_date'});
+combined_information = horzcat(combined_information, outputs_table);
 
 
-
-for i = 1 : length(files_to_load)
-    disp(files_to_load(i).name)
-    
-    temp = population_data.name(i);
-    disp(temp)
-    
-    
+for i = 1 : length(combined_information.name)
+    total_population = combined_information.populationServed(i);
+    file_to_load = [char(combined_information.folder(i)) '/' char(combined_information.name(i))];
+    [combined_information.R0(i), combined_information.R0_suppression(i), combined_information.start_date(i), combined_information.suppression_date(i)] = SEIR_MWE(num_initial_points, file_to_load, total_population);      
 end
+
+figure(12)
+subplot(2,2,1)
+histogram(combined_information.R0)
+subplot(2,2,2)
+histogram(combined_information.R0_suppression)
+subplot(2,2,3)
+histogram(combined_information.start_date)
+subplot(2,2,4)
+histogram(combined_information.suppression_date)
+
+
+
